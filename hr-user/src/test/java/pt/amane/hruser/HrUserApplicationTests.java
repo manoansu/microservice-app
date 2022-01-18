70d9c3ba557ae937d7d98ed257a78feceaa898c4
Ansumane Mané <manoansu@gmail.com>
2022-01-18 13:33:24 +0000

    project hr-user created

---
+++ hr-user/src/test/java/pt/amane/hruser/HrUserApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.amane.hruser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
