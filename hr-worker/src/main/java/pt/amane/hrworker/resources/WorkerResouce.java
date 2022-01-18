cb718ed8d29d2469ca49970d0506db485d5c6a6e
Ansumane Mané <manoansu@gmail.com>
2022-01-18 10:02:02 +0000

    Random port hr-payroll

--- hr-worker/src/main/java/pt/amane/hrworker/resources/WorkerResouce.java
+++ hr-worker/src/main/java/pt/amane/hrworker/resources/WorkerResouce.java
@@ -29,14 +29,7 @@ public class WorkerResouce {
 	
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<WorkerDTO> findById(@PathVariable Long id) {
-		
-		/*try {
-			Thread.sleep(3000L);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}*/
-		
+	
 		logger.info("PORT = " + env.getProperty("local.server.port"));
 		
 		WorkerDTO dto = service.findById(id);
