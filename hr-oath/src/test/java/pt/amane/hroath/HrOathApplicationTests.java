f331a1e440a9cba3403b86a6f4bda86106533621
Ansumane Mané <manoansu@gmail.com>
2022-01-18 16:57:22 +0000

    project hr-oath creeated

---
+++ hr-oath/src/test/java/pt/amane/hroath/HrOathApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.amane.hroath;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrOathApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
